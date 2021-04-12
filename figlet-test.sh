360f9981933bdbd9a252a0bd0391b43d5a57486c
Konishi <ke.konishi@leon-tec.co.jp>
2021-04-12 14:28:11 +0900

    pushed .sh file

---
+++ figlet-test.sh
@@ -0,0 +1,151 @@
+#!/bin/sh
+
+<< COMMENTOUT
+$NAME1=cola
+echo $NAME1;
+figlet -f $NAME1 -w 200 $WORD
+echo;
+COMMENTOUT
+
+WORD="security is key to growth"
+
+NAME_BLOCK="block"
+echo "$NAME_BLOCK\n"
+figlet -f $NAME_BLOCK -w 200 $WORD
+echo;
+
+NAME_CALGPHY2="calgphy2"
+echo "$NAME_CALGPHY2\n"
+figlet -f $NAME_CALGPHY2 -w 200 $WORD
+echo;
+
+NAME_COLA="cola"
+echo "$NAME_COLA\n"
+figlet -f $NAME_COLA -w 200 $WORD
+echo;
+
+NAME_COMPUTER="computer"
+echo "$NAME_COMPUTER\n"
+figlet -f $NAME_COMPUTER -w 200 $WORD
+echo;
+
+NAME_CYBERMEDIUM="cybermedium"
+echo "$NAME_CYBERMEDIUM\n"
+figlet -f $NAME_CYBERMEDIUM -w 200 $WORD
+echo;
+
+NAME_DOOM="doom"
+echo "$NAME_DOOM\n"
+figlet -f $NAME_DOOM -w 200 $WORD
+echo;
+
+NAME_FENDER="fender"
+echo "$NAME_FENDER\n"
+figlet -f $NAME_FENDER -w 200 $WORD
+echo;
+
+NAME_GRAFFITI="graffiti"
+echo "$NAME_FENDER\n"
+figlet -f $NAME_GRAFFITI -w 200 $WORD
+echo;
+
+NAME_HIEROGLYPHS="hieroglyphs"
+echo "$NAME_HIEROGLYPHS\n"
+figlet -f $NAME_HIEROGLYPHS -w 200 $WORD
+echo;
+
+NAME_HOLLYWOOD="hollywood"
+echo "$NAME_HOLLYWOOD\n"
+figlet -f $NAME_HOLLYWOOD -w 200 $WORD
+echo;
+
+NAME_LEAN="lean"
+echo "$NAME_LEAN\n"
+figlet -f $NAME_LEAN -w 200 $WORD
+echo;
+
+NAME_KBAN="kban"
+echo "$NAME_KBAN\n"
+figlet -f $NAME_KBAN -w 200 $WORD
+echo;
+
+
+NAME_OGRE="ogre"
+echo "$NAME_OGRE\n"
+figlet -f $NAME_OGRE -w 200 $WORD
+echo;
+
+NAME_PEPPER="pepper"
+echo "$NAME_PEPPER\n"
+figlet -f $NAME_PEPPER -w 200 $WORD
+echo;
+
+NAME_ROZZO="rozzo"
+echo "$NAME_ROZZO\n"
+figlet -f $NAME_ROZZO -w 200 $WORD
+echo;
+
+NAME_SANTACLARA="santaclara"
+echo "$NAME_SANTACLARA\n"
+figlet -f $NAME_SANTACLARA -w 200 $WORD
+echo;
+
+NAME_SCRIPT="script"
+echo "$NAME_SCRIPT\n"
+figlet -f $NAME_SCRIPT -w 200 $WORD
+echo;
+
+NAME_SHADOW="shadow"
+echo "$NAME_SHADOW\n"
+figlet -f $NAME_SHADOW -w 200 $WORD
+echo;
+
+NAME_SLANT="slant"
+echo "$NAME_SLANT\n"
+figlet -f $NAME_SLANT -w 200 $WORD
+echo;
+
+NAME_SPEED="speed"
+echo "$NAME_SPEED\n"
+figlet -f $NAME_SPEED -w 200 $WORD
+echo;
+
+NAME_STELLAR="stellar"
+echo "$NAME_STELLAR\n"
+figlet -f $NAME_STELLAR -w 200 $WORD
+echo;
+
+NAME_STOP="stop"
+echo "$NAME_STOP\n"
+figlet -f $NAME_STOP -w 200 $WORD
+echo;
+
+NAME_TREK="trek"
+echo "$NAME_TREK\n"
+figlet -f $NAME_TREK -w 200 $WORD
+echo;
+
+NAME_5LINEOBLIQUE="5lineoblique"
+echo "$NAME_5LINEOBLIQUE\n"
+figlet -f $NAME_5LINEOBLIQUE -w 200 $WORD
+echo;
+
+figlet -f eftitalic -w 200 $WORD;
+echo;
+figlet -f hollywood -w 200 $WORD;
+echo;
+figlet -f invita -w 200 $WORD;
+echo;
+figlet -f rowancap -w 200 $WORD;
+echo;
+figlet -f stampate -w 200 $WORD;
+echo;
+figlet -f slscript -w 200 $WORD;
+echo;
+figlet -f script -w 200 $WORD;
+echo;
+figlet -f morse -w 200 $WORD;
+echo;
+figlet -f larry3d -w 200 $WORD;
+echo;
+echo;figlet -f avatar -w 200 $WORD;
